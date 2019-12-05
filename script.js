c7e5ade98d28800a0b2fb0018c63d4ff27cd3904
bpaltezo08 <56845624+bpaltezo08@users.noreply.github.com>
2019-12-06 04:18:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,24 @@
 $(document).ready(function(){
+ 
 // updating the view with notifications using ajax
-function load_unseen_notification(view = '')
-{
+function load_unseen_notification(view = ''){
  $.ajax({
   url:"fetch.php",
   method:"POST",
   data:{view:view},
   dataType:"json",
-  success:function(data)
-  {
+  success:function(data){
    $('.dropdown-menu').html(data.notification);
-   if(data.unseen_notification > 0)
-   {
+   if(data.unseen_notification > 0){
     $('.count').html(data.unseen_notification);
    }
   }
  });
+
 }
-load_unseen_notification();
-// submit form and get new records
-$('#comment_form').on('submit', function(event){
- event.preventDefault();
- if($('#subject').val() != '' && $('#comment').val() != '')
- {
-  var form_data = $(this).serialize();
-  $.ajax({
-   url:"insert.php",
-   method:"POST",
-   data:form_data,
-   success:function(data)
-   {
-    $('#comment_form')[0].reset();
-    load_unseen_notification();
-   }
-  });
- }
- else
- {
-  alert("Both Fields are Required");
- }
-});
+
+ load_unseen_notification();
+ 
 // load new notifications
 $(document).on('click', '.dropdown-toggle', function(){
  $('.count').html('');
@@ -47,5 +26,5 @@ $(document).on('click', '.dropdown-toggle', function(){
 });
 setInterval(function(){
  load_unseen_notification();;
-}, 5000);
+}, 1000);
 });
